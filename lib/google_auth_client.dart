e46694c8fc6d30dca51ca999f10dbf70bcbccd32
BlackSardes <domonte08@gmail.com>
2025-12-28 19:34:10 -0300

    First commit

---
+++ lib/google_auth_client.dart
@@ -0,0 +1,21 @@
+import 'package:http/http.dart' as http;
+
+/// http.Client that adds Google auth headers to each request.
+class GoogleAuthClient extends http.BaseClient {
+  final Map<String, String> _headers;
+  final http.Client _inner = http.Client();
+
+  GoogleAuthClient(this._headers);
+
+  @override
+  Future<http.StreamedResponse> send(http.BaseRequest request) {
+    request.headers.addAll(_headers);
+    return _inner.send(request);
+  }
+
+  @override
+  void close() {
+    _inner.close();
+    super.close();
+  }
+}
